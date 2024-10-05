b3ccdec3293e928819dd6313bc84573139a63dcb
Oliver Lee <oliverlee333@gmail.com>
2024-10-05 04:39:28 -0400

    added table function

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from prem import *
 api_key = 'fa7bb5fd5de8461fb040abdb404b651a'
 headers = {'X-Auth-Token': api_key}
 
-
-get_table(headers)
+# testing different functions
+get_table(headers, 'PL')
--- prem.py
+++ prem.py
@@ -6,11 +6,11 @@ API_KEY = 'YOUR_API_KEY'
 # Headers for authentication
 headers = {'X-Auth-Token': API_KEY}
 
-def get_current_matchday(headers):
+def get_current_matchday(headers, league_ID):
     """
-    Fetches the current matchday for the Premier League
+    Fetches the current matchday for the given league
     """
-    url = 'https://api.football-data.org/v4/competitions/PL'
+    url = f'https://api.football-data.org/v4/competitions/{league_ID}'
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
@@ -18,14 +18,14 @@ def get_current_matchday(headers):
         current_matchday = data['currentSeason']['currentMatchday']
         return current_matchday
 
-def get_latest_premier_league_scores(headers):
+def get_latest_league_scores(headers, league_ID):
     """
-    Function that generates scores from the most recent PL matchweek.
+    Function that generates scores from the most recent league matchweek.
     When a new matchweek begins, the batch of scores is updated.
     """
-    current_matchday = get_current_matchday(headers)
+    current_matchday = get_current_matchday(headers, league_ID)
 
-    url = f'https://api.football-data.org/v4/competitions/PL/matches?matchday={current_matchday}'
+    url = f'https://api.football-data.org/v4/competitions/{league_ID}/matches?matchday={current_matchday}'
 
     response = requests.get(url, headers=headers)
 
@@ -45,16 +45,28 @@ def get_latest_premier_league_scores(headers):
 
             print(f"{match_date}: {home_team} {home_score} - {away_score} {away_team}")
 
-def get_table(headers):
+def get_table(headers, league_ID):
     """
-    Function that prints the current PL table
+    Function that prints the current league table
     """
-    url = 'https://api.football-data.org/v4/competitions/PL/standings'
+    url = f'https://api.football-data.org/v4/competitions/{league_ID}/standings'
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
 
-        for team in data['standings']:
-            print(team)
+        print(f"{'Position':<10}{'Team':<30}{'Points':<10}{'Wins':<10}{'Draws':<10}{'Losses':<10}{'Goal Difference':<15}")
+        print("=" * 95)
+
+        for standing in data['standings']:
+            for team in standing['table']:
+                pos = team['position']
+                name = team['team']['name']
+                points = team['points']
+                wins = team['won']
+                draws = team['draw']
+                losses = team['lost']
+                goal_diff = team['goalDifference']
+
+                print(f"{pos:<10}{name:<30}{points:<10}{wins:<10}{draws:<10}{losses:<10}{goal_diff:<15}")
